43572779ca2c806e60d15fe5e78350936d9de856
benmarita96@gmail.com <benmarita96@gmail.com>
2022-09-10 00:00:46 -0400

    document loaded

---
+++ js/index.js
@@ -0,0 +1,24 @@
+let form = document.querySelector("form")
+let searchBtn = document.getElementById("btn-search")
+let mealContainer = document.querySelector("#meals-container")
+let mealContent = document.getElementById("content")
+let searchRecipe = document.getElementById("recipe-up-btn")
+let searchMeals = document.getElementById("recipe-btn")
+let mealResult = document.getElementById("results")
+let countLikes = document.getElementById("like-count")
+let likeButton = document.getElementById("like-button")
+
+
+
+document.addEventListener("DOMContentLoaded",() =>{ form.addEventListener("submit",(e)=>{
+    
+    e.preventDefault();
+       const value = e.target.querySelector('input').value;
+       findMeals();
+   
+})
+   searchBtn.addEventListener("click", findMeals)
+   mealContainer.addEventListener('click',getRecipe)
+   searchRecipe.addEventListener("click",(e) => {
+   mealContent.parentElement.classList.remove('showRecipe');
+   
