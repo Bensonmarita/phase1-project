08b5ffdcc4f9b599cbeaae223c2a4c7dab1bb17f
benmarita96@gmail.com <benmarita96@gmail.com>
2022-09-10 00:09:48 -0400

    meals served

--- js/index.js
+++ js/index.js
@@ -10,88 +10,121 @@ let likeButton = document.getElementById("like-button")
 
 
 
-document.addEventListener("DOMContentLoaded",() =>{ form.addEventListener("submit",(e)=>{
+document.addEventListener("DOMContentLoaded",() =>{
+
+    form.addEventListener("submit",(e)=>{
     
-    e.preventDefault();
-       const value = e.target.querySelector('input').value;
-       findMeals();
-   
+     e.preventDefault();
+        const value = e.target.querySelector('input').value;
+        findMeals();
+    
 })
-   searchBtn.addEventListener("click", findMeals)
-   mealContainer.addEventListener('click',getRecipe)
-   searchRecipe.addEventListener("click",(e) => {
-   mealContent.parentElement.classList.remove('showRecipe');
+    searchBtn.addEventListener("click", findMeals)
+    mealContainer.addEventListener('click',getRecipe)
+    searchRecipe.addEventListener("click",(e) => {
+    mealContent.parentElement.classList.remove('showRecipe');
+    
+   })
+   likeButton.addEventListener('click', () => {
+      // alert(4)
+    let integer = 0;
+    integer += 1
+    countLikes.innerHTML = `${integer} likes`;
+    //getRecipeMeals()
+    
+    
+
 })
-likeButton.addEventListener('click', () => {
-   // alert(4)
- let integer = 0;
- integer += 1
- countLikes.innerHTML = `${integer} likes`;
- //getRecipeMeals()
+
+})
+     function findMeals(data){
+        let searchInput = document.getElementById("search-input").value.trim();
+    
+    
+    
+        fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInput}`)
+            
+    
+        .then(resp => resp.json())
+        .then(data=>{
+            //console.log(data)
+    
+
+  
+
  
- 
+     let data1=""
+     if(data.meals){
+     data.meals.forEach(meal => {
+        data1 +=  ` <div data-id="${meal.idMeal}"  class = "meal-item">
+                            <div class= "meal-image">
+                        
+                         <img src="${meal.strMealThumb}" class="img-rounded" id="image"> </div>
+                         <div class="meal-name">  
+                         <h3> ${meal.strMeal} </h3> 
+                            <a class = "recipe-btn">Get Recipe</a>
+                            
+                            
+                            
+                            </div>
+                        </div>
+                            `
+        
+    })
+                   mealContainer.classList.remove('not-found');
+}
+            else{
+                data1 = "Results not found"
+                mealContainer.classList.add('not-found');
+            }
+            mealContainer.innerHTML = data1; 
+            })
 
-})
+}
 
-})
-function findMeals(data){
-    let searchInput = document.getElementById("search-input").value.trim();
+                findMeals();
 
+                    function getRecipe(e){
+                        e.preventDefault();
+                        if(e.target.classList.contains('recipe-btn')){
+                            let mealItem = e.target.parentElement.parentElement;
+                        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+                        .then(response=> response.json())
+                        .then(data=> {
+                        // console.log(data)
+                        return getRecipeMeals(data.meals)
 
-
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInput}`)
+                        })
+                            
+        
         
 
-    .then(resp => resp.json())
-    .then(data=>{
-        //console.log(data)
-        let data1=""
-        if(data.meals){
-        data.meals.forEach(meal => {
-           data1 +=  ` <div data-id="${meal.idMeal}"  class = "meal-item">
-                               <div class= "meal-image">
-                           
-                            <img src="${meal.strMealThumb}" class="img-rounded" id="image"> </div>
-                            <div class="meal-name">  
-                            <h3> ${meal.strMeal} </h3> 
-                               <a class = "recipe-btn">Get Recipe</a>
-                               
-                               
-                               
-                               </div>
-                           </div>
-                            
-           
-       })
-                      mealContainer.classList.remove('not-found');
-                      
   
-                    }
+}
 
-                }
-                                    function getRecipeMeals(meal){
-                                        console.log(meal);
-                                        meal = meal[0];
-                                        let data1="";
-                                        data1 = `<h2 class = "recipe-title">${meal.strMeal}</h2>
-                                        <p class = "recipe-category">${meal.strCategory}</p>
-                                        <div class = "recipe-instruct">
-                                            <h3>Instructions:</h3>
-                                            <p>${meal.strInstructions}</p>
-                                        </div>
-                                        <div class = "recipe-meal-img">
-                                        <img src = "${meal.strMealThumb}" alt = "">
-                                    </div>
-                                    <div class = "recipe-link">
-                                        <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
-                                    </div>
-                                   
-                                    `
-                                   
-                                    mealContent.innerHTML=data1;
-                                    mealContent.parentElement.classList.add('showRecipe');
-                                   
-            
-            
-            }
-            
+}
+                    function getRecipeMeals(meal){
+                        console.log(meal);
+                        meal = meal[0];
+                        let data1="";
+                        data1 = `<h2 class = "recipe-title">${meal.strMeal}</h2>
+                        <p class = "recipe-category">${meal.strCategory}</p>
+                        <div class = "recipe-instruct">
+                            <h3>Instructions:</h3>
+                            <p>${meal.strInstructions}</p>
+                        </div>
+                        <div class = "recipe-meal-img">
+                            <img src = "${meal.strMealThumb}" alt = "">
+                        </div>
+                        <div class = "recipe-link">
+                            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
+                        </div>
+                       
+                        `
+                       
+                        mealContent.innerHTML=data1;
+                        mealContent.parentElement.classList.add('showRecipe');
+                       
+
+
+}
